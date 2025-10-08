69efeff321c4d78435abf326817f02c0ae91f238
Binh To <binhtod1152019@gmail.com>
2025-10-07 18:46:44 -0700

    completed lab

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -46,17 +46,6 @@ const users = {
     ]
 };
 
-// Part 6
-// const addUser = (user) => {
-//     users["users_list"].push(user);
-//     return user;
-// };
-
-// app.post("/users", (req, res) => {
-//     const userToAdd = req.body;
-//     const addedUser = addUser(userToAdd);
-//     res.send(201).jason(addedUser);
-// });
 
 
 // Part 4
@@ -66,73 +55,97 @@ const findUserByName = (name) => {
     );
 };
 
-// const findUserByJob = (job) => {
-//     return users["users_list"].filter(
-//         (user) => user["job"] === job
-//     );
-// };
+const findUserByJob = (job) => {
+    return users["users_list"].filter(
+        (user) => user["job"] === job
+    );
+};
 
-// app.get("/users", (req, res) => {
-//     res.send(users);
-// });
+const findUserById = (id) =>
+    users["users_list"].find((user) => user["id"] === id);
 
-// Part 5
-// const findUserById = (id) =>
-//     users["users_list"].find((user) => user["id"] === id);
+// Part 6
+const addUser = (user) => {
+    users["users_list"].push(user);
+    return user;
+};
 
-// // Part 7 task 1 (modified part 5)
-// app.delete("/users/:id", (req, res) => {
-//     const id = req.params["id"];
-//     let result = findUserById(id);
-//     if (result == undefined) {
-//         res.status(404).send("User not found.");
-//     } else {
-//         users["users_list"] = users["users_list"].filter(user => user.id !== id);
-//         res.json(result);
-//     }
-// });
+// Random ID generator
+const generateRandomID = () => {
+    return Math.random().toString(36).substring(2, 6);
+};
 
+// POST: Add a new user with random ID
+app.post("/users", (req, res) => {
+    const userToAdd = req.body;
 
+    // assign random ID to user
+    userToAdd.id = generateRandomID();
+
+    const addedUser = addUser(userToAdd);
+    if (addedUser == undefined) {
+        return res.status(400).send("Bad request");
+    } else {
+        // returns the new object, including the randomly generated ID
+        res.status(201).json(addedUser);
+    }
+
+});
+
+// DELETE:
+app.delete("/users/:id", (req, res) => {
+    const id = req.params["id"];
+    let result = findUserById(id);
+    if (result == undefined) {
+        res.status(404).send("Resource not found\n");
+    } else {
+        users["users_list"] = users["users_list"].filter(user => user.id !== id);
+        res.status(204).json(result);
+    }
+});
+
+// GET by ID
 app.get("/users/:id", (req, res) => {
     const id = req.params["id"]; //or req.params.id
     let result = findUserById(id);
     if (result === undefined) {
-        res.status(404).send("Resource not found.");
+        res.status(404).send("Resource not found\n");
     } else {
         res.send(result);
     }
 });
 
 // Part 4
-app.get("/users", (req, res) => {
-    const name = req.query.name;
-    if (name != undefined) {
-        let result = findUserByName(name);
-        result = { users_list: result };
-        res.send(result);
-    } else {
-        res.send(users);
-    }
-});
+// app.get("/users", (req, res) => {
+//     const name = req.query.name;
+//     if (name != undefined) {
+//         let result = findUserByName(name);
+//         result = { users_list: result };
+//         res.send(result);
+//     } else {
+//         res.send(users);
+//     }
+// });
 
-// Part 7 Task 2
+// GET all 
 app.get("/users", (req, res) => {
-    console.log("✅ Received request from frontend for /users");
     const name = req.query.name;
     const job = req.query.job;
+    let filteredUsers = users["users_list"];
 
+    // Check for error
     if (name !== undefined && job === undefined) {
         return res.status(400).json({
             error: "If 'name' is provided, 'job' must also be provided."
         });
     }
-    let filteredUsers = users["users_list"];
 
-    if (name != undefined) {
-        filteredUsers = filteredUsers.filter(user => user.name == name);
+    // Filter by name/job
+    if (name !== undefined) {
+        filteredUsers = filteredUsers.filter(user => user.name === name);
     }
-    if (job != undefined) {
-        filteredUsers = filteredUsers.filter(user => user.job == job);
+    if (job !== undefined) {
+        filteredUsers = filteredUsers.filter(user => user.job === job);
     }
-    res.json({ users: filteredUsers });
+    res.json({ users_list: filteredUsers });
 });
